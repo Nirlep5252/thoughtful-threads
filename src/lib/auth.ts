9925be5a5622a5e31aedf04700d55032a9d7f713
Nirlep <hello@nirlep.dev>
2024-07-07 15:54:57 +0530

    feat: add email magic link auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,5 @@
 import Google from "@auth/core/providers/google";
+import Nodemailer from "@auth/core/providers/nodemailer";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
@@ -11,6 +12,17 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST!,
+        port: process.env.EMAIL_SERVER_PORT!,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER!,
+          pass: process.env.EMAIL_SERVER_PASSWORD!,
+        },
+      },
+      from: process.env.EMAIL_FROM!,
     }),
   ],
 });
