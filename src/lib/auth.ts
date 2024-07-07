3acd0051155f3ae0f825739c320cabfaf17c9c2b
Nirlep <hello@nirlep.dev>
2024-07-07 14:45:11 +0530

    feat: auth + shadcn + prisma

---
+++ src/lib/auth.ts
@@ -0,0 +1,16 @@
+import Google from "@auth/core/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+
+const prisma = new PrismaClient();
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
